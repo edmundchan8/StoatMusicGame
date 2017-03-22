4fe13702df91afb698d98225d6ca4228facd79da
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-03-21 19:17:10 -0700

    Basic menu scene, loads into game scene, menu and game music playing and works

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	[SerializeField]
+	Text m_Text;
+
+	[SerializeField]
+	float m_Timer;
+
+	void Start () 
+	{
+		m_Timer = Time.timeSinceLevelLoad;
+	}
+
+	void Update () 
+	{
+		m_Timer += Time.deltaTime;
+		m_Text.text = "Time: " + m_Timer.ToString("F2");
+	}
+}
