36f2ff2dc3adc698c7b4c42c3f136cbc5df0413d
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-03-27 18:19:14 -0700

    Music now set by times on a notepad file.  Modifications to screen space - camera, combo meter and modifications to range for excellenct, good etc, using transform.localposition now.

---
+++ Assets/Scripts/ComboScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ComboScript : MonoBehaviour 
+{
+	[Header ("Accessor")]
+	[SerializeField]
+	TouchPanel m_TouchPanel;
+	[SerializeField]
+	Text m_Text;
+
+	void Start()
+	{
+		gameObject.SetActive(false);
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		m_Text.text = m_TouchPanel.GetCombo() + " hit combo!";
+	}
+}
--- Assets/Scripts/NoteMove.cs
+++ Assets/Scripts/NoteMove.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class NoteMove : MonoBehaviour 
 {
-	float m_Speed = 2f;
+	float m_Speed = 2.1f;
 
-	void FixedUpdate() 
+	void Update() 
 	{
-		transform.Translate(Vector2.left * m_Speed * Time.deltaTime);
+		transform.Translate(Vector2.left * m_Speed * Time.deltaTime, Space.Self);
 	}
 
 }
