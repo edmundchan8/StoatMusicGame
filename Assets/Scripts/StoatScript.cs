702a2b4fdc97ef4282433111a10a1d957baa6d77
Chi Hong Edmund Chan <pokedev8@gmail.com>
2017-05-12 17:13:18 -0700

    #19-Edge closer - Hard coded times for stoat to move towards rabbit, only works for level 1 due to hard coding.

--- Assets/Scripts/StoatScript.cs
+++ Assets/Scripts/StoatScript.cs
@@ -14,16 +14,38 @@ public class StoatScript : MonoBehaviour
 	[SerializeField]
 	float m_CurrentTime;
 
+	[Header ("Accessor")]
+	[SerializeField]
+	GameObject m_MusicManagerObject;
+	[SerializeField]
+	MusicManager m_MusicManagerScript;
+
+	[Header ("Stoat Move Times")]
+	[SerializeField]
+	float[] m_Level01MoveTimeArr = new float[]{32f, 64f};
+
+	[SerializeField]
+	int m_ArrayCounter=0;
+
 	void Start()
 	{
 		m_StartPos = transform.position;
 		m_EndPos = transform.position;
+		m_MusicManagerScript = m_MusicManagerObject.GetComponent<MusicManager>();
 	}
 
 	void Update()
 	{
 		m_CurrentTime += Time.deltaTime;
 		MoveCloserRabbit();
+		if (m_ArrayCounter < 2)
+		{
+			if (m_MusicManagerScript.GetCurrentMusicTime() > m_Level01MoveTimeArr[m_ArrayCounter])
+			{
+				SetLerpPositions();
+				m_ArrayCounter++;
+			}
+		}
 	}
 
 	public void MoveCloserRabbit() 
